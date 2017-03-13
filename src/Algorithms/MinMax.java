87e9b2f120e5164c9f553ed743d82746f4030af9
Abhishek Vichare <abhishekk6690@gmail.com>
2017-03-12 23:32:34 -0500

    Update MinMax.java

--- src/Algorithms/MinMax.java
+++ src/Algorithms/MinMax.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class MinMax {
 	  public static void main(String[] args) {
 		  Scanner in = new Scanner(System.in);
-	        long max = Long.MAX_VALUE;
-	        long min = Long.MIN_VALUE;
+	        long min = Long.MAX_VALUE;
+	        long max = Long.MIN_VALUE;
 	        long sum = 0;
 	        System.out.println("Enter the numbers");
 	        for(int i=0; i<5; i++){
