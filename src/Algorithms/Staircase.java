fee41096557b24f5da29b78d2bd925f2efaa6e69
Abhishek Vichare <abhishekk6690@gmail.com>
2017-03-12 22:51:13 -0500

    Create Staircase.java

---
+++ src/Algorithms/Staircase.java
@@ -0,0 +1,17 @@
+package Algorithms;
+
+public class Staircase {
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    for(int i=0; i<6 ; i++){
+			for(int j=i; j<5; j++){
+				System.out.print(" ");
+    
+			}
+			for(int j=0; j<=i; j++){
+		System.out.print("#");
+    }
+			System.out.println();
+    }}
+
+}
