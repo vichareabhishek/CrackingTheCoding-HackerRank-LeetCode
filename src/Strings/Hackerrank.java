78a72d75c8878390dd84c7005366fdbb93412eea
abhishekk6690 <abhishekk6690@gmail.com>
2017-04-03 18:01:47 -0500

    updating with added problems

---
+++ src/Strings/Hackerrank.java
@@ -0,0 +1,28 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Hackerrank {
+	public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+		String hack = "hackerrank";
+        for(int a0 = 0; a0 < q; a0++){
+          String s = scan.next();
+	
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<hack.length()){
+        	if(s.charAt(i)==hack.charAt(j)){
+        		i++;
+        		j++;
+        	}else
+        		i++;
+        } 	
+        if(j==10)
+        	System.out.println("YES");
+        else
+        	System.out.println("No");
+        }
+	}
+}
