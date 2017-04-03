982c6624345ffb025eed108227ca06180071495e
abhishekk6690 <abhishekk6690@gmail.com>
2017-04-03 16:44:45 -0500

    create

---
+++ src/Arrays/Reverse.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.io.*;
+import java.util.*;
+
+public class Reverse {
+    public static void main(String[] args) {
+        
+    
+  Scanner input=new Scanner(System.in);
+  System.out.println("Enter numbers");
+int length=input.nextInt();
+int[] values=new int[length];
+System.out.println("Enter numbers");
+for(int i=0; i<length; i++){
+    values[i]=input.nextInt();
+}
+
+
+
+
+}
+}
